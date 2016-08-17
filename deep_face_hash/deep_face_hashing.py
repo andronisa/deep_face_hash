7716a9a773d18609592af2e7a3919751e57b8f8b
Antonios Andronis <aa3e15@soton.ac.uk>
2016-08-17 04:09:29 +0100

    Fix CRLF

--- deep_face_hash/deep_face_hashing.py
+++ deep_face_hash/deep_face_hashing.py
@@ -14,78 +14,87 @@ from vgg16 import generate_feature_maps, load_model
 from utils import top_n_closer_vecs, top_n_closer_hash_codes, top_n_hamm_hash_codes, find_common_in_lists
 
 
-def hash_lfw(fpath='/home/aandronis/scikit_learn_data/lfw_home/lfw/', window_size=None, hash_size=None, reset_db=False,
+def hash_lfw(fpath='/home/aandronis/scikit_learn_data/lfw_home/lfw/', reset_db=False,
              existing_maps=False):
+
+    bit_sizes = [64, 128, 256, 512, 1024, 2048, 4096]
+
+    min_window_size = 2800
+    max_window_size = 5000
+    step = 100
+
+    # Current ---> 2700 for all
+    # max_window_size+step is to include end (max_window_size)
+
     print("\n##################### INITIALIZE MODEL #########################")
     lfw_model = load_model()
 
-    # compute one feature map to get dimensionality
-    hash_vars = generate_hash_vars(model=lfw_model, window=window_size, bits=hash_size)
-
-    # careful!!
-    print("\n##################### DATABASE SETUP #########################")
-    col_name = "_".join(("hash_maps", str(window_size), str(hash_size), "bit"))
-    feat_map_collection = "feature_maps"
-
-    if reset_db:
-        clear_collection(col_name)
     if not existing_maps:
         print("DELETED FEATURE MAPS COLLECTION! Re-preprocessing starting..")
         clear_collection('feature_maps')
-
-    print("\n#################### CHUNK HANDLING ##########################")
-    print("\nStarting Hashing...")
+    else:
+		feature_maps = map(pickle.loads,
+                       [item['feature_map'] for item in
+                        mongodb_find({}, {'feature_map': 1}, None, collection="feature_maps")])
 
     (chunked_img_paths, chunked_targets, chunked_names, img_options) = load_lfw_db(fpath)
-    if existing_maps:
-        print("Using Preprocessed Feature Maps...")
+    for window_size in range(min_window_size, max_window_size + step, step):
+        for hash_size in bit_sizes:
+		    # compute one feature map to get dimensionality
+		    hash_vars = generate_hash_vars(model=lfw_model, window=window_size, bits=hash_size)
 
-        feature_maps = map(pickle.loads,
-                           [item['feature_map'] for item in
-                            mongodb_find({}, {'feature_map': 1}, None, collection=feat_map_collection)])
-        hash_codes = generate_hash_maps(feature_maps, hash_vars, window_size, hash_size)
-        targets = list(itertools.chain.from_iterable(chunked_targets))
-        names = list(itertools.chain.from_iterable(chunked_names))
-        batch_list = zip(hash_codes, targets, names)
+		    # careful!!
+		    print("\n##################### DATABASE SETUP #########################")
+		    col_name = "_".join(("hash_maps", str(window_size), str(hash_size), "bit"))
 
-        db_keys = ['hash_code', 'target', 'name']
-        col_name = "_".join(("hash_maps", str(window_size), str(hash_size), "bit"))
-        mongodb_store(batch_list, db_keys, col_name)
+		    if reset_db:
+		        clear_collection(col_name)
 
-        del targets
-        del names
-        del hash_codes
-        del feature_maps
-        del batch_list
-    else:
-        print("Total batches: " + str(img_options['n_batch']))
-        print("Images per batch: " + str(img_options['num_faces']))
+		    print("\n#################### CHUNK HANDLING ##########################")
+		    print("\nStarting Hashing...")
+		    if existing_maps:
+		        hash_codes = generate_hash_maps(feature_maps, hash_vars, window_size, hash_size)
+		        targets = list(itertools.chain.from_iterable(chunked_targets))
+		        names = list(itertools.chain.from_iterable(chunked_names))
+		        batch_list = zip(hash_codes, targets, names)
 
-        batch_counter = 0
-        for img_paths in chunked_img_paths:
-            print("\nStarting image batch no." + str(batch_counter + 1) + "\n")
-            print("Preprocessing Images...")
+		        db_keys = ['hash_code', 'target', 'name']
+		        col_name = "_".join(("hash_maps", str(window_size), str(hash_size), "bit"))
+		        mongodb_store(batch_list, db_keys, col_name)
 
-            preprocessed_images = preprocess_images(img_paths.tolist(), img_options=img_options)
-            feature_maps = generate_feature_maps(preprocessed_images, lfw_model)
-            hash_codes = generate_hash_maps(feature_maps, hash_vars, window_size, hash_size)
-            batch_list = zip(hash_codes, chunked_targets[batch_counter].tolist(),
-                             chunked_names[batch_counter].tolist())
+		        del targets
+		        del names
+		        del hash_codes
+		        del batch_list
+		    else:
+		        print("Total batches: " + str(img_options['n_batch']))
+		        print("Images per batch: " + str(img_options['num_faces']))
 
-            db_keys = ['hash_code', 'target', 'name']
-            col_name = "_".join(("hash_maps", str(window_size), str(hash_size), "bit"))
-            mongodb_store(batch_list, db_keys, col_name)
+		        batch_counter = 0
+		        for img_paths in chunked_img_paths:
+		            print("\nStarting image batch no." + str(batch_counter + 1) + "\n")
+		            print("Preprocessing Images...")
 
-            del preprocessed_images
-            del feature_maps
-            del hash_codes
-            del batch_list
+		            preprocessed_images = preprocess_images(img_paths.tolist(), img_size=(224, 224), img_options=img_options)
+		            feature_maps = generate_feature_maps(preprocessed_images, lfw_model)
+		            hash_codes = generate_hash_maps(feature_maps, hash_vars, window_size, hash_size)
+		            batch_list = zip(hash_codes, chunked_targets[batch_counter].tolist(),
+		                             chunked_names[batch_counter].tolist())
 
-            print("\n##############################################")
-            print("Finished image batch no." + str(batch_counter + 1))
-            print("##############################################\n")
+		            db_keys = ['hash_code', 'target', 'name']
+		            col_name = "_".join(("hash_maps", str(window_size), str(hash_size), "bit"))
+		            mongodb_store(batch_list, db_keys, col_name)
 
-            batch_counter += 1
+		            del preprocessed_images
+		            del feature_maps
+		            del hash_codes
+		            del batch_list
+
+		            print("\n##############################################")
+		            print("Finished image batch no." + str(batch_counter + 1))
+		            print("##############################################\n")
+
+		            batch_counter += 1
 
     del chunked_img_paths
     del chunked_names
@@ -159,23 +168,13 @@ def deep_face_hashing(fpath, print_names=False):
 
 
 def generate_multiple_hash_map_collections():
-    bit_sizes = [64, 128, 256, 512, 1024, 2048, 4096]
-
-    min_window_size = 2100
-    max_window_size = 5000
-    step = 100
-
-    # Current ---> for all until 5000
-    # max_window_size+step is to include end (max_window_size)
-    for window_size in range(min_window_size, max_window_size + step, step):
-        for bits in bit_sizes:
-            hash_lfw(window_size=window_size, hash_size=bits, reset_db=True, existing_maps=True)
+    hash_lfw(reset_db=True, existing_maps=True)
 
 
 if __name__ == '__main__':
     # win_size = calculate_mean_window_size()
     # hash_lfw(fpath='/home/aandronis/scikit_learn_data/lfw_home/lfw/', window_size=win_size, hash_size=64,
     #          reset_db=True,
-    #          existing_maps=False)
+    #          existing_maps=None)
     # deep_face_hashing(fpath='/home/aandronis/projects/deep_face_hash/deep_face_hash/data/img/test/', print_names=True)
     generate_multiple_hash_map_collections()
